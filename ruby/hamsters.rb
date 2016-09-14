c6e288c560031d126c442462a56d3c7d5755ec85
Kim Tran <Kimathytran@gmail.com>
2016-09-14 00:21:33 -0700

    assign variables to hamster.rb

---
+++ ruby/hamsters.rb
@@ -0,0 +1,29 @@
+# asking for hamster's name
+puts "What's the hamster's name?"
+name = gets.chomp
+puts "Hi,#{name}!"
+
+#asking for user's volume tolerance
+puts "What's your volume tolerance, from 1-10?"
+volume = gets.chomp
+puts "Your volume tolerance is #{volume}."
+
+#Fur color preference
+puts "What fur color do you prefer?"
+color = gets.chomp
+puts "You prefer, #{color}!"
+
+#Whether the hamster is a good candidate for adoption
+healthy = true
+if healthy
+	puts "Yes, adopt me."
+end.
+else
+	puts "Don't adopt me, let me die a painful lonely death."
+
+#Estimate age
+puts "What's the hamster's age?"
+age = gets.chomp
+else
+	puts age = nil
+puts "Your hamster is #{age} years old!"
