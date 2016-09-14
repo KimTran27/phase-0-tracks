4b36267852ade68282736ca244c1775172367d14
Kim Tran <Kimathytran@gmail.com>
2016-09-14 15:16:19 -0700

    add name and volume level for hamster

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -4,26 +4,30 @@ name = gets.chomp
 puts "Hi,#{name}!"
 
 #asking for user's volume tolerance
-puts "What's your volume tolerance, from 1-10?"
+puts "What's the hamster's volume level, from 1-10?"
 volume = gets.chomp
-puts "Your volume tolerance is #{volume}."
+puts "Hamster's volume level is #{volume}."
 
 #Fur color preference
-puts "What fur color do you prefer?"
+puts "What's it fur color?"
 color = gets.chomp
-puts "You prefer, #{color}!"
+puts "#{color}!"
 
 #Whether the hamster is a good candidate for adoption
-healthy = true
-if healthy
-	puts "Yes, adopt me."
-end.
-else
-	puts "Don't adopt me, let me die a painful lonely death."
+puts "Is the hamster a good candidate for adoption? (y/n)"
+answer = gets.chomp
+
+if answer == "y"
+	puts "Hamster is a good candidate."
+else 
+	puts "Hamster is not a good candiate for adoption."
+end
 
 #Estimate age
 puts "What's the hamster's age?"
-age = gets.chomp
-else
-	puts age = nil
-puts "Your hamster is #{age} years old!"
+if estimated_age = empty.to_i
+	puts nil
+else 
+	estimated_age = age.to_i
+	puts "#{estimated_age}"
+end.
