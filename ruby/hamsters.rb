5ea0e1c6a1c44c7e2ba3b1f2773307c1f2a0c90b
Kim Tran <Kimathytran@gmail.com>
2016-09-15 14:35:29 -0700

    add estimated age question tohamster questionaire

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,15 +1,22 @@
 # asking for hamster's name
 puts "What's the hamster's name?"
-name = gets.chomp
-puts "Hi,#{name}!"
+hamster_name = gets.chomp
+hamster_name = hamster_name.capitalize
+puts "Hi,#{hamster_name}!"
 
 #asking for user's volume tolerance
 puts "What's the hamster's volume level, from 1-10?"
 volume = gets.chomp
+volume = volume.to_i
+if volume < 5
+	volume = "silent"
+elsif volume >= 5
+	volume = "noisy"
+end
 puts "Hamster's volume level is #{volume}."
 
 #Fur color preference
-puts "What's it fur color?"
+puts "What's the fur color?"
 color = gets.chomp
 puts "#{color}!"
 
@@ -18,16 +25,25 @@ puts "Is the hamster a good candidate for adoption? (y/n)"
 answer = gets.chomp
 
 if answer == "y"
-	puts "Hamster is a good candidate."
-else 
-	puts "Hamster is not a good candiate for adoption."
+	puts "Good candidate."
+else answer == "n"
+	puts "Not a good candidate."
 end
 
 #Estimate age
-puts "What's the hamster's age?"
-if estimated_age = empty.to_i
-	puts nil
-else 
-	estimated_age = age.to_i
-	puts "#{estimated_age}"
-end.
+puts "What is the hamster's estimated age?"
+	estimated_age = gets.chomp
+	if estimated_age ==""
+		estimated_age = "N/A"
+	else
+		estimated_age = estimated_age.to_f
+end
+
+puts "-------------------------"
+puts "Data Summary:"
+puts "Name: #{hamster_name}"
+puts "Noise Level: #{volume}"
+puts "Color: #{color}"
+puts "Suitability: #{answer}"
+puts "Age: #{estimated_age}"
+puts "-------------------------"
