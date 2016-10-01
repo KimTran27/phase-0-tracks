30c3650983e8517c0867ef55d167d0677eabf9ce
Kim <Kimathytran@gmail.com>
2016-10-01 10:26:27 -0700

    create a method list

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,1 +1,48 @@
-hello, it's me.
+# Method to print a list and make it look nice
+
+def print_list(list)
+	list.each do |item,quantity|
+		puts "I am purchasing #{quantity} #{item}."
+	end
+end
+
+def list(string)
+	array = string.split(" ")
+	hash = Hash.new
+	array.each do |item|
+		hash[item] = 1
+	end
+	return
+	p hash
+	print_list(hash)
+end
+#grocery_list = list("carrots oranges kale lettuce spinach mushrooms")
+#default quantity = 1
+
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # [fill in any steps here]
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here, array or hash?]
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps:
+# output:
+
+# Method to remove an item from the list
+# input:
+# steps:
+# output:
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
+
