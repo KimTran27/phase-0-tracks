07d4f284b0f4a8fa62ab3cf5ad90cd5d14784492
Kim <Kimathytran@gmail.com>
2016-10-09 16:58:30 -0700

    add explore.js

--- js/explore.js
+++ js/explore.js
@@ -1,1 +1,19 @@
 // create a function with one parameter
+function sayHello(string){
+// set the empty string named new_string
+	new_string = ""
+	// use loop to prepend the each letter of the string to new_str
+	// repeat this until the loop hits the last letter of the string
+	for (i = 0 ; i < string.length ; i++){
+		new_string = string[i] + new_string
+	}
+
+	return new_string
+}
+
+if (true) {
+	console.log(sayHello("hello"));	
+}
+else {
+	console.log(sayHello("goodbye"));
+}
