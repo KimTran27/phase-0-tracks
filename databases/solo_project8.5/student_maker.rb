76348bb486136d2987417eb39d3e1d495ef29e08
Kim <Kimathytran@gmail.com>
2016-11-28 09:21:46 -0800

    add find student method

--- databases/solo_project8.5/student_maker.rb
+++ databases/solo_project8.5/student_maker.rb
@@ -23,3 +23,26 @@ db.execute(create_table_cmd)
 def create_student(db,student_id,name,major,email)
 	db.execute("INSERT INTO students (student_id,name,major,email) VALUES (?,?,?,?)",[student_id,name,major,email])
 end
+
+def find_student(db,attr,value)
+	puts "##### Results #####"
+
+	case attr
+	when "student_id"
+	result = db.execute("SELECT student_id,name,major,email FROM students WHERE student_id = ?",[value])
+
+	when "name"
+	result = db.execute("SELECT student_id,name,major,email FROM students WHERE name = ?",[value])
+
+	when "major"
+	result = db.execute("SELECT student_id,name,major,email FROM students WHERE major = ?",[value])
+
+	when "email"
+	result = db.execute("SELECT student_id,name,major,email FROM students WHERE email = ?",[value])
+
+	else
+		puts "INVALID INPUT"
+	end
+	puts "#{result[0]['student_id']} | #{result[0]['name']} | #{result[0]['major']} "
+	puts "email : #{result[0]['email']}"
+end
