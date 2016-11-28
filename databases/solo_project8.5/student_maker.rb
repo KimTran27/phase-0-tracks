d839cf4aead223d3ed2664970c362b574721b229
Kim <Kimathytran@gmail.com>
2016-11-28 09:17:26 -0800

    add fancy string delimiters

--- databases/solo_project8.5/student_maker.rb
+++ databases/solo_project8.5/student_maker.rb
@@ -1,6 +1,18 @@
+# require gems
 require 'sqlite3'
 require 'faker'
 
 #create sqlite3 database
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true;
+
+# Using string delimeter to create string, to add to data
+create_table_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS students(
+		id INTEGER PRIMARY KEY,
+		student_number INTEGER,
+		name VARCHAR(255),
+		major VARCHAR(255),
+		email VARCHAR(255)
+	)
+SQL
